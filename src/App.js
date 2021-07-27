81c5d4c5030a6345bc97a089c8830c5dc27b94e6
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-27 16:41:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './GirlReading.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>The start of the Dictionary App</h1>
+      </header>
+      <small>This app is coded by Rebecca Yates and <a href="#">is open-sourced on Github</a>, and is currently a work in progress.</small>
+      </div>
+    </div>
+  );
+}
