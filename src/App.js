ee099493072fbfb13f15873d74cc17248379226c
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-27 17:09:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ export default function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>The start of the Dictionary App</h1>
       </header>
-      <small>This app is coded by Rebecca Yates and <a href="https://github.com/rebyates121/dictionary-project" target="_blank" rel="noreferrer">is open-sourced on Github</a>, and is currently a work in progress.</small>
+      <small>
+        This app is coded by Rebecca Yates and 
+        <a href="https://github.com/rebyates121/dictionary-project" target="_blank" rel="noreferrer">
+          is open-sourced on Github
+        </a>
+          , and is currently a work in progress.
+      </small>
       </div>
     </div>
   );
