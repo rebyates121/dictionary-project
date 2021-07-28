2189ad9e35aae6cb9f7244af11ffc7ef956141e0
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-28 15:30:54 +0100

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import logo from './GirlReading.svg';
-import './App.css';
+import logo from "./GirlReading.svg";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>The start of the Dictionary App</h1>
-      </header>
-      <small>
-        This app is coded by Rebecca Yates and 
-        <a href="https://github.com/rebyates121/dictionary-project" target="_blank" rel="noreferrer">
-          is open-sourced on Github
-        </a>
-          , and is currently a work in progress.
-      </small>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Dictionary</h1>
+        </header>
+        <Dictionary />
+        <footer>
+          <small>
+            This app is coded by Rebecca Yates and{" "}
+            <a
+              href="https://github.com/rebyates121/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              is open-sourced on Github
+            </a>
+            , and is currently a work in progress.
+          </small>
+        </footer>
       </div>
     </div>
   );
