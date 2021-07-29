a861c532617600c67d6932913a1d51001a633608
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-29 15:51:45 +0100

    Implemented api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition...`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
