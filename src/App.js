544253bf8d76a52afbea910442725bde45c624c7
rebyates121 <84729304+rebyates121@users.noreply.github.com>
2021-07-30 19:50:34 +0100

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -11,20 +11,20 @@ export default function App() {
           <h1>Dictionary</h1>
         </header>
         <Dictionary />
-        <footer>
-          <small>
-            This app is coded by Rebecca Yates and{" "}
-            <a
-              href="https://github.com/rebyates121/dictionary-project"
-              target="_blank"
-              rel="noreferrer"
-            >
-              is open-sourced on Github
-            </a>
-            , and is currently a work in progress.
-          </small>
-        </footer>
       </div>
+      <footer>
+        <small>
+          This app is coded by Rebecca Yates and{" "}
+          <a
+            href="https://github.com/rebyates121/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            is open-sourced on Github
+          </a>
+          , and is currently a work in progress.
+        </small>
+      </footer>
     </div>
   );
 }
